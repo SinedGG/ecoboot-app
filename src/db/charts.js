2cdca9a11582f2285ac67bc1d8b288cc5d66ced9
Denis <51417134+SinedGG@users.noreply.github.com>
2023-11-21 16:18:59 +0200

    Change SQL to view

--- src/db/charts.js
+++ src/db/charts.js
@@ -5,20 +5,7 @@ function chart1(start, end) {
   return new Promise((resolve) => {
     db.any(
       `
-    SELECT
-    s.City AS Область,
-    MAX(CASE WHEN mu.Title = 'PM2.5' THEN m.Value END) AS PM2_5,
-    MAX(CASE WHEN mu.Title = 'PM10' THEN m.Value END) AS PM10
-FROM
-    Measurement m
-JOIN
-    Station s ON m.ID_Station = s.ID_Station
-JOIN
-    measured_unit mu ON m.ID_Measured_Unit = mu.ID_Measured_Unit
-WHERE
-    m.Time BETWEEN '${start}' AND '${end}'
-GROUP BY
-    s.City
+    SELECT * FROM GetAirQualityData('${start}', '${end}');
     `
     ).then((result) => {
       resolve({
@@ -31,25 +18,7 @@ GROUP BY
 
 function chart2(station) {
   return new Promise((resolve) => {
-    db.any(
-      `
-      SELECT
-      'Показник PM2.5' as title,
-    COUNT(CASE WHEN avg_value BETWEEN 30 AND 55 THEN 1 ELSE NULL END) AS count_between_30_and_55,
-    COUNT(CASE WHEN avg_value BETWEEN 55 AND 110 THEN 1 ELSE NULL END) AS count_between_55_and_110
-FROM (
-    SELECT
-        DATE(Time) AS measurement_date,
-        ID_Station,
-        AVG(Value) AS avg_value
-    FROM Measurement
-    WHERE ID_Station = '${station}' AND ID_Measured_Unit = '3'
-    GROUP BY measurement_date, ID_Station
-) AS subquery;
-
-      
-    `
-    ).then((result) => {
+    db.any(`SELECT * FROM get_pm25_counts('${station}');`).then((result) => {
       resolve({
         header: ["Показник", "Шкідливий", "Дуже шкідливий"],
         data: result,
@@ -60,26 +29,16 @@ FROM (
 
 function chart3() {
   return new Promise((resolve) => {
-    db.any(
-      `
-      SELECT
-      'SO2' as title, 
-        SUM(CASE WHEN value BETWEEN 0 AND 20 THEN 1 ELSE 0 END) AS good,
-        SUM(CASE WHEN value BETWEEN 20 AND 50 THEN 1 ELSE 0 END) AS moderate,
-      SUM(CASE WHEN value BETWEEN 50 AND 100 THEN 1 ELSE 0 END) AS poor,
-      SUM(CASE WHEN value BETWEEN 100 AND 200 THEN 1 ELSE 0 END) AS verypoor
-    FROM Measurement
-    WHERE ID_Measured_Unit = '13';
-      
-    `
-    ).then((result) => {
+    db.any(`SELECT * FROM SO2_Measurement_View;`).then((result) => {
       resolve({
         header: [
           "Показник",
+          "Відмінний",
           "Добрий",
-          "Задовільний",
+          "Помірний",
           "Поганий",
           "Дуже поганий",
+          "Жахливий",
         ],
         data: result,
       });
@@ -88,20 +47,17 @@ function chart3() {
 }
 function chart4() {
   return new Promise((resolve) => {
-    db.any(
-      `
-      SELECT
-      'CO' as title, 
-        SUM(CASE WHEN value BETWEEN 0 AND 9 THEN 1 ELSE 0 END) AS good,
-        SUM(CASE WHEN value BETWEEN 9 AND 25 THEN 1 ELSE 0 END) AS moderate,
-      SUM(CASE WHEN value BETWEEN 25 AND 100 THEN 1 ELSE 0 END) AS poor
-    FROM Measurement
-    WHERE ID_Measured_Unit = '12';
-      
-    `
-    ).then((result) => {
+    db.any(`SELECT * FROM СO_Measurement_View;`).then((result) => {
       resolve({
-        header: ["Показник", "Добрий", "Задовільний", "Поганий"],
+        header: [
+          "Показник",
+          "Відмінний",
+          "Добрий",
+          "Помірний",
+          "Поганий",
+          "Дуже поганий",
+          "Жахливий",
+        ],
         data: result,
       });
     });
